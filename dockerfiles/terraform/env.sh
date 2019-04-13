8f181fff504eb903d78ca6f767e6a86165a056db
i3c <i3c@o2.pl>
2019-04-13 08:56:33 +0000

    secrets handler

---
+++ dockerfiles/terraform/env.sh
@@ -0,0 +1,4 @@
+
+export TF_VAR_aws_access_key=$(/r secret awsAccessKey)
+export TF_VAR_aws_secret_key=$(/r secret awsSecretKey)
+export TF_VAR_aws_region=$(/r secret awsRegion)
--- dockerfiles/terraform/i3c-run.sh
+++ dockerfiles/terraform/i3c-run.sh
@@ -1,3 +1,10 @@
 
 
-dParams="-d "
+dParams="-d \
+    --secret awsAccessKey \
+    --secret awsSecretKey \
+    --secret awsRegion \
+    "
+i3cAfter() {
+   ## /i exshd terraform "echo \"export TF_VAR_aws_access_key=\\\$(/r secret awsAccessKey)\" >> /root/.bashrc"
+}    
