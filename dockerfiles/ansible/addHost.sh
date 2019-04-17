3c8873ca74db839372d9b312a1bd881dc24a8142
mb <mb@test.pl>
2019-04-17 09:31:55 +0000

    fixes

--- dockerfiles/ansible/addHost.sh
+++ dockerfiles/ansible/addHost.sh
@@ -6,10 +6,13 @@ fi
 if [ "x$2" == "x" ]; then
     echo "Usage: /addHost [accSecretKey] [host]";
     exit;
-fi
+ficd
 if [ ! -e /run/secrets/sshKeys/$1 ]; then
     echo "File /run/secrets/sshKeys/$1 not found";
     exit;
 fi
 
-cat ~/.ssh/id_rsa.pub | ssh -i /run/secrets/sshKeys/$1 ansible@$2 'cat >> .ssh/authorized_keys && echo "Key copied"'
+sudo sh -c "echo \"\\n$2\" >> /etc/ansible/hosts"
+
+cat ~/.ssh/id_rsa.pub | ssh -i /run/secrets/sshKeys/$1 ansible@$2 'cat >> .ssh/authorized_keys && echo "Host added."'
+
